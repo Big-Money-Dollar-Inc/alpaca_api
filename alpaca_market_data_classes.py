3be7fc5f927da5254a6d3c4c1bebd222cb965da5
Luna <luna.meerkats@gmail.com>
2026-01-04 19:29:56 +1100

    Added market data classes

---
+++ alpaca_market_data_classes.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class HistoricalAuctions:
+    pass
+
+
+@dataclass
+class HistoricalBars:
+    pass
+
+
+@dataclass
+class LatestBars:
+    pass
+
+
+@dataclass
+class ConditionCodes:
+    pass
+
+
+@dataclass
+class ExchangeCodes:
+    pass
+
+
+@dataclass
+class HistoricalQuotes:
+    pass
+
+
+@dataclass
+class LatestQuotes:
+    pass
+
+
+@dataclass
+class Snapshots:
+    pass
+
+
+@dataclass
+class HistoricalTrades:
+    pass
+
+
+@dataclass
+class LatestTrades:
+    pass
+
+
+@dataclass
+class MostActiveStocks:
+    pass
+
+
+@dataclass
+class TopMarketMovers:
+    pass
